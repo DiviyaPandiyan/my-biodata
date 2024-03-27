8441cea0e300b6fb8db2e58928a2a88fea5feb2a
K.Diviyapandiyan <162093025+DiviyaPandiyan@users.noreply.github.com>
2024-03-27 09:28:00 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const navMenu=document.getElementById('nav-menu'),
+      toggleMenu=document.getElementById('nav-toggle'),
+      closeMenu=document.getElementById('nav-close')
+      
+
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show')
+})
+
+const navLink=document.querySelectorAll('.nav__link')
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const sections =document.querySelectorAll('section[id]')
+window.addEventListener('scroll',scrollActive)
+
+function scrollActive(){
+    const sectionHeight = current.offsetHeight
+    const sectionTop=current.offsetTop-50
+    sectionid=current.getAttribute('id')
+    
+    if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+        document.querySelector('nav__menu a[herf*='+sectionid+']').classList.add('active')
+    }
+    else{
+        document.querySelector('nav__menu a[herf*='+sectionid+']').classList.remove('active')
+    }
+
+
+}
